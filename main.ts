de1cc033f697b009f28383bb1b0dbe0c7101781d
Christian Antonsen <christian.antonsen@gmail.com>
2022-09-11 23:39:36 +0200

    Lagt til kommentarer til koden. Gir bedre forklaring.

--- main.ts
+++ main.ts
@@ -1,14 +1,19 @@
+// Input: Trykker på knapp A for å redusere verdi på variabelen Teller.
 input.onButtonPressed(Button.A, function () {
     Teller += -1
 })
+// Input: Trykker på knapp A for å øke verdi på variabelen Teller.
 input.onButtonPressed(Button.B, function () {
     Teller += 1
 })
+// Input: Riste på Microbit for å nullstille.
 input.onGesture(Gesture.Shake, function () {
     Teller = 0
 })
+// Her starter programmet. Dette er det første som kjøres.
 let Teller = 0
 Teller = 0
+// Med en gang on Start er ferdig, kjøres denne løkken helt til programmet avsluttes.
 basic.forever(function () {
     if (Teller < 0) {
         Teller = 0
